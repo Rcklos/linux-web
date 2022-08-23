acf9613231c055930389f2b0626dac1b4586eae2
rcklos <2362819691@qq.com>
2022-08-23 09:05:43 +0800

    完善了simple client

--- base0t/io_multiplexing/lib/src/kit/sock.cpp
+++ base0t/io_multiplexing/lib/src/kit/sock.cpp
@@ -1,14 +1,11 @@
 #include "kit/sock.h"
 #include "log.h"
-#include <arpa/inet.h>
-#include <asm-generic/socket.h>
-#include <string.h>
-#include <netinet/in.h>
 #include <assert.h>
+#include <netinet/in.h>
+#include <sys/select.h>
 #include <sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<errno.h>
+#include <unistd.h>
+
 
 int fish::create_socket_tcp_default(int port, socket_type_t type) {
   // 这里应该是client，暂不实现
@@ -57,28 +54,24 @@ fish::socket_t fish::create_socket_tcp_default(socket_type_t type, int port) {
   return sock;
 }
 
-fish::socket_addr_t* fish::create_socket_addr(char *ip, int port) {
-  if(!ip && port < 0) return NULL;
-  fish::socket_addr_t *addr = new fish::socket_addr_t();
-  addr->ip = ip;
-  addr->port = port;
-  struct sockaddr_in *sockaddr = new struct sockaddr_in();
-  memset(sockaddr, 0, sizeof(struct sockaddr_in));
-  sockaddr->sin_family = AF_INET;
-  sockaddr->sin_port = port;
-  sockaddr->sin_addr.s_addr = ip? inet_addr(ip): INADDR_ANY;
+// fish::socket_addr_t* fish::create_socket_addr(char *ip, int port) {
+//   if(!ip && port < 0) return NULL;
+//   fish::socket_addr_t *addr = new fish::socket_addr_t();
+//   addr->ip = ip;
+//   addr->port = port;
+//   struct sockaddr_in *sockaddr = new struct sockaddr_in();
+//   memset(sockaddr, 0, sizeof(struct sockaddr_in));
+//   sockaddr->sin_family = AF_INET;
+//   sockaddr->sin_port = port;
+//   sockaddr->sin_addr.s_addr = ip? inet_addr(ip): INADDR_ANY;
+//
+//   addr->sockaddr = sockaddr;
+//   return addr;
+// }
 
-  addr->sockaddr = sockaddr;
-  return addr;
-}
 
-
-void fish::free_socket_addr(socket_addr_t *addr) {
-  if(addr->sockaddr)
-    delete (struct sockaddr_in*)addr->sockaddr;
-  delete addr;
-}
-
-inline int fish::connect(socket_t sockfd, socket_addr_t *addr) {
-  return connect(sockfd, (struct sockaddr*)addr->sockaddr, sizeof(struct sockaddr_in));
-}
+// void fish::free_socket_addr(socket_addr_t *addr) {
+//   if(addr->sockaddr)
+//     delete (struct sockaddr_in*)addr->sockaddr;
+//   delete addr;
+// }
--- base0t/io_multiplexing/test/simple_client/client.cpp
+++ base0t/io_multiplexing/test/simple_client/client.cpp
@@ -1,13 +1,12 @@
 #include "kit/sock.h"
 #include "log.h"
-#include <cassert>
-#include <cstdlib>
-#include <cstring>
+#include <assert.h>
+#include <cerrno>
+#include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
 
 #define BUFFER_SIZE 1024
-
-using namespace fish;
 
 int main(int argc, char **argv) {
   const char *ip = "127.0.0.1";
@@ -15,21 +14,24 @@ int main(int argc, char **argv) {
   if(argc >=  2) port = atoi(argv[2]);
   if(argc >=  3) ip = argv[1];
 
-  socket_t sockfd = fish::create_socket_tcp_default(SOCKET_TYPE_SEND);
-  socket_addr_t *addr = fish::create_socket_addr(const_cast<char *>(ip), port);
+  fish::socket_t sockfd = fish::create_socket_tcp_default(fish::SOCKET_TYPE_SEND);
+  fish::socket_addr_t *addr = new fish::socket_addr_t(const_cast<char *>(ip), port);
+
+  LOGD("连接服务器[%s:%d]...", ip, port);
   int ret = fish::connect(sockfd, addr);
+  LOGD("sockefd[%d] -------> %s", sockfd, strerror(errno));
   assert(ret != -1);
 
   std::string str;
-  // char buffer[BUFFER_SIZE];
-  // bzero(buffer, BUFFER_SIZE);
-  // 后面还没写
   while(1) {
+    printf("输入消息: ");
     std::cin >> str;
     LOGD("发送消息: %s", str.c_str());
     send(sockfd, str.c_str(), str.length(), 0);
 
-    if(std::strstr(str.c_str(), "stop__")) break;
+    if(strstr(str.c_str(), "stop__")) break;
   }
   LOGD("bye");
+  delete addr;
+  close(sockfd);
 }
